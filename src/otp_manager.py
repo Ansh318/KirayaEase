bc7c71fda976c57c0ae2dee1868f15fbe0d69676
Ansh318 <aragarwal@wisc.edu>
2025-09-04 04:38:57 -0400

    updated database

--- src/otp_manager.py
+++ src/otp_manager.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 from fastapi import HTTPException
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+import requests
+import psycopg2
 # Load environment variables
 load_dotenv()
 
@@ -34,7 +35,7 @@ class OTPManager:
             "password": os.getenv("SMTP_PASSWORD"),
             "sender": os.getenv("SMTP_SENDER"),
         }
-        self.db_path = os.getenv("DATABASE_PATH")
+        self.db_path = os.getenv("DATABASE_URL")
 
     def generate_otp(self):
         return str(random.randint(100000, 999999))
@@ -42,25 +43,26 @@ class OTPManager:
     def store_otp(self, user_id, otp, validity_seconds=7000):
         expiry = int(time.time()) + validity_seconds
         session_token = str(uuid.uuid4())
-        with sqlite3.connect(self.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(STORE_OTP,(user_id, otp, session_token, expiry))
-            conn.commit()
+        conn = psycopg2.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor = conn.cursor()
+        cursor.execute(STORE_OTP,(user_id, otp, session_token, expiry))
+        conn.commit()
         return session_token
 
     def create_fetch_user(self, email):
-        with sqlite3.connect(self.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(CREATE_USER, (email,))
-            conn.commit()
+        conn = psycopg2.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute(CREATE_USER, (email,))
+        conn.commit()
 
-            cursor.execute(FETCH_USER, (email,))
-            result = cursor.fetchone()
+        cursor.execute(FETCH_USER, (email,))
+        result = cursor.fetchone()
 
-            if result:
-                return result[0]
-            else:
-                raise ValueError("Failed to fetch or create user")
+        if result:
+            return result[0]
+        else:
+            raise ValueError("Failed to fetch or create user")
 
     def send_email(self, receiver_email, otp):
         try:
@@ -87,39 +89,39 @@ class OTPManager:
 
     def verify_otp(self, session_token, user_otp):
         current_time = int(time.time())
-        with sqlite3.connect(self.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(VERIFY_OTP, (session_token,))
-            result = cursor.fetchone()
-            if not result:
-                raise ValueError("No valid OTP found for this session")
-            
-            user_id, otp, expiry, = result[0], result[1], result[2]
-            if current_time > expiry or otp != user_otp:
-                raise ValueError("OTP has expired or is invalid")
-            
-            cursor.execute(UPDATE_OTP, (session_token, user_id)) 
-            conn.commit()
-            return user_id
+        conn = psycopg2.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute(VERIFY_OTP, (session_token,))
+        result = cursor.fetchone()
+        if not result:
+            raise ValueError("No valid OTP found for this session")
+        
+        user_id, otp, expiry, = result[0], result[1], result[2]
+        if current_time > expiry or otp != user_otp:
+            raise ValueError("OTP has expired or is invalid")
+        
+        cursor.execute(UPDATE_OTP, (session_token, user_id)) 
+        conn.commit()
+        return user_id
 
     def create_login_session(self, user_id):
         session_id = str(uuid.uuid4())
-        with sqlite3.connect(self.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(AUTH_SESSION, (session_id, user_id))
-            conn.commit()
+        conn = psycopg2.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute(AUTH_SESSION, (session_id, user_id))
+        conn.commit()
         return session_id
 
     def check_onboarding(self, user_id):
-        with sqlite3.connect(self.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(CHECK_ONBOARDED, (user_id,))
-            result = cursor.fetchone()
-            if result:
-                id, role = result[0], result[1]
-                return True, role
-            else:
-                return False, 'None'
+        conn = psycopg2.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute(CHECK_ONBOARDED, (user_id,))
+        result = cursor.fetchone()
+        if result:
+            id, role = result[0], result[1]
+            return True, role
+        else:
+            return False, 'None'
 
                 
 # otp_manager = OTPManager()
